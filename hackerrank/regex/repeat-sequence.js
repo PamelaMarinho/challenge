6ba51045a8b436bf50e6797b9680ceebab78fc21
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-06 15:59:31 -0300

    repeat-sequence

---
+++ hackerrank/regex/repeat-sequence.js
@@ -0,0 +1,6 @@
+// Task: You have a test String .
+// Your task is to write a regex which will match  with the following condition:
+
+//  should have  or more consecutive repetitions of ok.
+
+var Regex_Pattern = /(ok){3,}/;
