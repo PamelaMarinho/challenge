c2d4839962bd8a97044d726723337611d85de698
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-06 12:34:33 -0300

    digit-non-digit

---
+++ hackerrank/regex/digit-non-digit.js
@@ -0,0 +1,5 @@
+// Task: You have a test string . Your task is to match the pattern 
+// Here  denotes a digit character, and  denotes a non-digit character.
+
+
+const regex_pattern = /\d{2}\D{1}\d{2}\D{1}\d{4}/
