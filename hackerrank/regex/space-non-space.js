9cfeb212218fc84c0ccdcb22219aad8ca91ffe36
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-06 12:39:21 -0300

    space-non-space

---
+++ hackerrank/regex/space-non-space.js
@@ -0,0 +1,4 @@
+// Task: You have a test string . Your task is to match the pattern 
+// Here,  denotes whitespace characters, and  denotes non-white space characters.
+
+var Regex_Pattern = /\S{2}\s\S{2}\s\S{2}/
