3df62917bb3c4c5559062dfc350974117f117a62
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-06 11:15:49 -0300

    sqrt

--- teste.js
+++ teste.js
@@ -1,4 +1,13 @@
-const arr2 = [4,4]
-const a = arr2.map(e=>e*2)
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+// Note: The function accepts an integer and returns an integer
 
-console.log(!!null,!![], !!true)
+function squareDigits(num){
+    const arr = num.toString().match(/[0-9]/g)
+    const sqrtArray = arr.map( num => {
+        return num*num
+    })
+    return parseInt(sqrtArray.join(""))
+}
+
+console.log(squareDigits(3212))
