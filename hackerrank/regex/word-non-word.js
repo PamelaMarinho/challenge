295fabd27716c3a529b2cc42fe1b35dd581bd344
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-06 12:43:38 -0300

    word-non-word

---
+++ hackerrank/regex/word-non-word.js
@@ -0,0 +1,4 @@
+// Task: You have a test string . Your task is to match the pattern 
+// Here  denotes any word character and  denotes any non-word character.
+
+var Regex_Pattern = /\w{3}\W\w{10}\W\w{3}/;
