4456a05304152908c869267a5a036f95851b5d77
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-06 13:17:10 -0300

    word-digit

---
+++ hackerrank/regex/word-digit.js
@@ -0,0 +1,5 @@
+// Task: You have a test string . Your task is to match the pattern 
+// Here,  denotes a word character, and  denotes a digit.  must start with a digit  and end with . symbol.
+//  should be  characters long only.
+
+var Regex_Pattern = /^\d\w{4}\.$/;
