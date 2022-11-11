9b8d5403925bc75eed4cdabc0a34b5174aeb17b5
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-10 23:13:31 -0300

    non-newline

---
+++ hackerrank/regex/non-newline.js
@@ -0,0 +1,4 @@
+// Task: You have a test string . Your task is to write a regular expression that matches only and
+//  exactly strings of form: , where each variable  can be any single character except the newline.
+
+var Regex_Pattern = /^(...\.){3}...$/;
