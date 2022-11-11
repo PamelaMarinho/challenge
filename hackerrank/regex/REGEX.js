5015af52175e87d7f5870eab1b8e0d40641f51c2
Pamela Marinho <pamelamarinhopm@hotmail.com>
2022-11-10 21:24:20 -0300

    regex-resume

---
+++ hackerrank/regex/REGEX.js
@@ -0,0 +1,30 @@
+// \n newline character
+// \r carriage return 
+// \t tab
+// \0 null character
+// [[:alnum:]] letters and digits
+// [[:alpha:]] letters
+// [[:ascii]] ascII codes 0-127
+// [[:blank]] space or tab only
+// [[:cntrl]] control characters
+// [[:digit:]] decimal digits
+// [[:graph:]] visible characters
+// [[:punct:]] visible punctuation characters
+// [[:space:]] whitespace
+// [[:word:]] word characters
+// [[:xdigit:]] hexadecimal digit character
+// [[:<:]] start od word character
+// [[:>:]] end of word character
+// a|b alternate either a or b character
+// \s space character
+// \S any non space character
+// \d any digit character
+// \D any non digit character
+// \w any word character
+// \W any non word character
+// \X any unicode sequences, linebreaks included
+// \C data unit character
+// \R newlines character
+// \N anything but a newline
+// \v vertical whitespace
+// \V negation of \v
